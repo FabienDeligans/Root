89caae84ea885cb21782c8e743a52886e4a99a0e
Fabien Deligans <fabien.deligans@ingeli.fr>
2023-11-03 09:10:28 +0100

    randomEnum

--- Common/Helper/RandomHelper.cs
+++ Common/Helper/RandomHelper.cs
@@ -16,10 +16,13 @@
             return new string(randomChar); 
         }
 
+        private static Random _random = new Random(10);
+        public static bool UseRandom = true;
         public static T GetRandomEnum<T>() where T : Enum
         {
+            var rand = UseRandom ? new Random() : _random;
             var values = Enum.GetValues(typeof(T));
-            return (T)values.GetValue(new Random().Next(values.Length))!; 
+            return (T)values.GetValue(rand.Next(values.Length))!;
         }
     }
 }
