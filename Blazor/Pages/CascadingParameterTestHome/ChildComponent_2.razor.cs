9b5a4cacf705b8e242430b39d3b4281a7d0872a2
Deligans <Fabien>
2024-01-08 22:37:18 +0100

    Cascading parameter

---
+++ Blazor/Pages/CascadingParameterTestHome/ChildComponent_2.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.CascadingParameterTestHome
+{
+    public partial class ChildComponent_2
+    {
+        [CascadingParameter(Name = "2_Parameter")]
+        public string Parameter_2 { get; set; }
+    }
+}
