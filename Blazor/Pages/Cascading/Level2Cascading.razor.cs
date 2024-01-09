415a22528da4c86b6e3b3f9cfe0ab8cc63dbfbea
Fabien Deligans <fabien.deligans@ingeli.fr>
2024-01-09 08:49:52 +0100

    test CascadingParameter

---
+++ Blazor/Pages/Cascading/Level2Cascading.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Pages.Cascading
+{
+    public partial class Level2Cascading
+    {
+        [CascadingParameter(Name = "ValueCascaded1")]
+        public string CascadedValue { get; set; }
+
+        [CascadingParameter(Name = "ValueCascaded2")]
+        public string CascadedValue2 { get; set; }
+    }
+}
---
+++ Blazor/Pages/Cascading/TestCascading.razor.cs
@@ -0,0 +1,10 @@
+﻿using Common.Helper;
+
+namespace Blazor.Pages.Cascading
+{
+    public partial class TestCascading
+    {
+        public string CascadedValue1 { get; set; } = RandomHelper.GetRandomString(10);
+        public string CascadedValue2 { get; set; } = RandomHelper.GetRandomString(10);
+    }
+}
